2b4b5614e34e75d64a6abdc384a039b802d14842
Matthew King <mattking73@gmail.com>
2019-12-08 23:14:08 -0800

    Day 2

--- day-2/app.js
+++ day-2/app.js
@@ -1,7 +1,48 @@
 const readlines = require('../readlines');
-const result = '';
+let part1 = 0;
+let part2 = 0;
+
+const opcodeCalc = (nums) => {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] === 1) {
+      nums[nums[i+3]] = nums[nums[i+1]] + nums[nums[i+2]];
+    } else if (nums[i] === 2) {
+      nums[nums[i+3]] = nums[nums[i+1]] * nums[nums[i+2]];
+    } else if (nums[i] === 99) {
+      break;
+    }
+    i += 4;
+  }
+  return nums[0];
+}
+
+const calcFirstPair = (nums) => {
+  const cloned = nums.slice(0);
+  cloned[1] = 12;
+  cloned[2] = 2;
+  part1 = opcodeCalc(cloned);
+};
+
+const calcExpectedPair = (nums, expected) => {
+  for (let i = 0; i < 100; i++) {
+    for (let k = 0; k < 100; k++) {
+      const cloned = nums.slice(0);
+      cloned[1] = i;
+      cloned[2] = k;
+      const result = opcodeCalc(cloned);
+      if (result === expected) {
+        part2 = 100 * i + k;
+        return;
+      };
+    }
+  }
+};
 
 readlines((line) => {
+  const nums = line.split(',').map((n) => parseInt(n, 10));
+  calcFirstPair(nums);
+  calcExpectedPair(nums, 19690720);
 }, (printResults) => {
-  printResults(result);
+  printResults([part1, part2]);
 });
